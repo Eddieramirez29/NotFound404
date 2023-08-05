38e61c3e3ef74f62191f33f00766750abfe8d0c5
Eduardo <tkd13000@gmail.com>
2023-08-04 20:21:53 -0600

    Proyecto terminado

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const buttonMain = document.getElementById("button");
 
 
-buttonMain.addEventListener("click", function()
-{
-    window.location.href = "http://127.0.0.1:5500/index.html"; //Obtiene el id del botón
-})
+buttonMain.addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5500/index.html";
+});
 
-
-const buttonBack = document.getElementsByClassName("container-button")[0];
-
-
-buttonBack.addEventListener("click", function()
-{
-    window.location.href = "http://127.0.0.1:5500/main.html"; //Obtiene el id del botón
-})
+buttonBack.addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5500/main.html";
+});
---
+++ app2.js
@@ -0,0 +1,5 @@
+const buttonBack = document.getElementById("buttonBack");
+
+buttonBack.addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:5500/main.html";
+});
